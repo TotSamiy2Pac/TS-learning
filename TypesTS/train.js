5b9872c0b1eedddb55a6d6311991c364f1364575
TotSamiy2Pac <a.r.a.p.o.v@yandex.ru>
2025-02-01 20:17:11 +0300

    type of ts

---
+++ TypesTS/train.js
@@ -0,0 +1,8 @@
+var a = 5;
+var b = "Hello TypeScript";
+var c = false;
+var a2 = Number('333');
+var b2 = String(32342);
+console.log(c, typeof c);
+console.log(a2, typeof a2);
+console.log(b2, typeof b2);
---
+++ TypesTS/train.ts
@@ -0,0 +1,10 @@
+const a: number = 5;
+const b: string = "Hello TypeScript";
+const c: boolean = false;
+const a2: number = Number('333');
+const b2: string = String(32342);
+
+
+console.log(c, typeof c)
+console.log(a2, typeof a2)
+console.log(b2, typeof b2)
